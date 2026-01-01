a9908d571388b7777f9e0e9e21a09b8405dd3431
Anand6262 <pyexpert62@gmail.com>
2026-01-01 20:43:57 +0530

    changes

--- src/utils/Apis.js
+++ src/utils/Apis.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://jwtapi1111.onrender.com/api" || "http://localhost:5000/api",
+  baseURL: "https://jwtapi1111.onrender.com/api" || "http://localhost:3000/api",
   withCredentials: true, // refresh token in cookie
 });
 
